5437c1fd26f821b9d4569961ab3dc5eefc3da749
eldarjahic <eldarjahic.00@hotmail.com>
2021-03-16 14:02:55 +0100

    added dao files

--- api/dao/BaseDao.class.php
+++ api/dao/BaseDao.class.php
@@ -1,34 +1,79 @@
 <?php
 require_once dirname(__FILE__)."/../config.php";
 
-class BaseDao {
-  private $connection;
 
-  public function __construct(){
+class BaseDao {
+  protected $connection;
+
+  private $table;
+
+  public function __construct($table){
+    $this->table = $table;
     try {
       $this->connection = new PDO("mysql:host=".Config::DB_HOST.";dbname=".Config::DB_SCHEME, Config::DB_USERNAME, Config::DB_PASSWORD);
       $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      echo "Connected";
     } catch(PDOException $e) {
-      echo "Connection failed: " . $e->getMessage();
+      throw $e;
     }
   }
 
-  public function insert(){
+  protected function insert($table, $entity){
+    $query = "INSERT INTO ${table} (";
+    foreach ($entity as $column => $value) {
+      $query .= $column.", ";
+    }
+    $query = substr($query, 0, -2);
+    $query .= ") VALUES (";
+    foreach ($entity as $column => $value) {
+      $query .= ":".$column.", ";
+    }
+    $query = substr($query, 0, -2);
+    $query .= ")";
 
+    $stmt= $this->connection->prepare($query);
+    $stmt->execute($entity); 
+    $entity['id'] = $this->connection->lastInsertId();
+    return $entity;
   }
 
-  public function update(){
+  protected function execute_update($table, $id, $entity, $id_column = "id"){
+    $query = "UPDATE ${table} SET ";
+    foreach($entity as $name => $value){
+      $query .= $name ."= :". $name. ", ";
+    }
+    $query = substr($query, 0, -2);
+    $query .= " WHERE ${id_column} = :id";
 
+    $stmt= $this->connection->prepare($query);
+    $entity['id'] = $id;
+    $stmt->execute($entity);
   }
 
-  public function query(){
-    // SELECT * FROM users WHERE id = 7;
+  protected function query($query, $params){
+    $stmt = $this->connection->prepare($query);
+    $stmt->execute($params);
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
   }
 
-  public function query_unique(){
-
+  protected function query_unique($query, $params){
+    $results = $this->query($query, $params);
+    return reset($results);
   }
 
+  public function add($entity){
+    return $this->insert($this->table, $entity);
+  }
+
+  public function update($id, $entity){
+    $this->execute_update($this->table, $id, $entity);
+  }
+
+  public function get_by_id($id){
+    return $this->query_unique("SELECT * FROM ".$this->table." WHERE id = :id", ["id" => $id]);
+  }
+
+  public function get_all($offset = 0, $limit = 25){
+    return $this->query("SELECT * FROM ".$this->table." LIMIT ${limit} OFFSET {$offset}", []);
+  }
 }
 ?>
