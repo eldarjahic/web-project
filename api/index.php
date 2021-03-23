83dbc0d7dc2efd604a9f4d10869ec0efd02e0f0e
eldarjahic <eldarjahic.00@hotmail.com>
2021-03-23 15:01:22 +0100

    added flight php and routes for users

--- api/index.php
+++ api/index.php
@@ -1,8 +1,50 @@
 <?php
+require_once dirname(__FILE__)."/dao/UserDao.class.php";
+require dirname(__FILE__). '/../vendor/autoload.php';
+Flight::register('userDao', 'UserDao');
+Flight::route('GET /users', function(){
 
-require_once dirname(__FILE__)."/dao/BaseDao.class.php";
+  $user=Flight::userDao()->get_all(0,10);
+  Flight::json($user);
 
-$db = new BaseDao();
+});
+Flight::route('GET /users/@id', function($id){
 
-echo "Hello from API";
+  $user=Flight::userDao()->get_by_id($id);
+  Flight::json($user);
+
+
+});
+Flight::route("POST /users", function(){
+  $request = Flight::request();
+  $data = $request->data->getData();
+
+  $user = Flight::userDao()->add($data);
+  Flight::json($user);
+
+
+
+
+
+  });
+
+
+Flight::route("PUT /users/@id", function($id){
+  $request = Flight::request();
+  $data = $request->data->getData();
+
+  Flight::userDao()->update($id,$data);
+  $user = Flight::userDao()->get_by_id($id);
+
+
+
+
+  Flight::json($user);
+
+  });
+
+
+  Flight::start();
+
+
  ?>
--- api/test.php
+++ api/test.php
@@ -5,16 +5,14 @@ error_reporting(E_ALL);
 
 require_once dirname(__FILE__)."/dao/UserDao.class.php";
 require_once dirname(__FILE__)."/dao/GoalsDao.class.php";
-$dao = new GoalsDao();
+$dao = new UserDao();
 
-$goal = [
-  "user_id"=>1,
-  "goal" => "smrsati 5 kg",
-  "status" => "ne znamo status",
-  "recycled" => 0
-];
 
-$goal1 = $dao->add($goal);
 
-print_r($goal1);
+
+$goal1 = $dao->get_all($_GET["offset"], $_GET["limit"]);
+echo json_encode($goal1, JSON_PRETTY_PRINT);
+
+
+
  ?>
